7c1227910bc1d7eb24aef1979aec170bc351d6db
Dave Voutila <voutilad@gmail.com>
2022-09-26 14:07:10 -0400

    version bump: release new deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="checker",
-    version="0.2.0",
+    version="0.3.0",
 
     url="https://github.com/neo4j-field/checker",
     maintainer="Dave Voutila",
