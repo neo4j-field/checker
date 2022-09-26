10ecdedd7b2b5661e9fd8b7ffb968ce479cc8c71
Dave Voutila <voutilad@gmail.com>
2022-09-26 13:58:21 -0400

    bump version, fix deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="checker",
-    version="0.1.0",
+    version="0.2.0",
 
     url="https://github.com/neo4j-field/checker",
     maintainer="Dave Voutila",
@@ -12,6 +12,7 @@ setup(
     install_requires=[
         "pandas>=1.4.0",
 	"pyarrow>=9.0.0",
+	"graphdatascience>=1.3.0",
     ],
     packages=find_packages(),
 )
